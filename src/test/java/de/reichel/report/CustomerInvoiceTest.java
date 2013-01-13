25bfd80d17a23c9767e15c2bc6176f5435fbd2f4
Alastair Calderwood <alastair.tux@gmail.com>
2013-01-13 12:26:08 +0000

    Outline of reparatur screen - combines Protokol and Auftrag

--- src/test/java/de/reichel/report/CustomerInvoiceTest.java
+++ src/test/java/de/reichel/report/CustomerInvoiceTest.java
@@ -4,12 +4,6 @@
  */
 package de.reichel.report;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.junit.Test;
 
 
@@ -22,12 +16,5 @@ public class CustomerInvoiceTest {
     @Test
     public void testGenerateReport() throws Exception {
 
-        List<TestBean> list = new ArrayList<TestBean>();
-        JRBeanCollectionDataSource src = new JRBeanCollectionDataSource(list);
-        Map parameters = new HashMap();
-        parameters.put("ReportTitle", "Address Report");
-        parameters.put("DataFile", "TestBean.java");
-        JasperFillManager.fillReportToFile("reports/reparatur_2.jasper",
-                parameters, src);
     }
 }
